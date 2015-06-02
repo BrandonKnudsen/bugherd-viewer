dae1fabb2b48873a134f8ec6466d64d820fd01fe
Brandon Knudsen <bk@generationlabs.com>
2015-06-01 23:02:13 -0600

    Main Script
    Shows all tasks in BugHerd

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+<?php
+/**
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   BugHerd
+ * @package    BugHerd Viewer
+ * @author     Generation Labs https://github.com/GenerationLabs
+ * @copyright  2015 Generation Labs LLC
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    1.0
+ * @link        https://github.com/GenerationLabs/bugherd-viewer
+ */
